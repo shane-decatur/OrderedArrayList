89f22f47466e47b078e5a4fce035242454bb1e4c
shane-decatur <sdecatur20@stuy.edu>
2020-12-10 18:04:20 -0500

    added constructors

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,3 +1,12 @@
+import java.util.*;
 public class NoNullArrayList<T> extends ArrayList<T>{
+  public NoNullArrayList(){
+    super();
+  }
+
+  public NoNullArrayList(int initialCapacity){
+    super(initialCapacity);
+  }
+
   
 }
