272929b077bffce89a11db24b0ed412169aa97ee
shane-decatur <sdecatur20@stuy.edu>
2020-12-13 19:21:53 -0500

    fixed add() and set() methods

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,6 +8,9 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   }
 
   public boolean add(T element){
+    if (element == null){
+      throw new IllegalArgumentException();
+    }
     int index = 0;
     for (int i = 0; i < size(); i++){
       Comparable<T> tempElement = get(i);
@@ -23,5 +26,14 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
 
   public void add(int index,T element){
     add(element);
+  }
+
+  public T set(int index, T element){
+    if (element == null){
+      throw new IllegalArgumentException();
+    }
+    T tempElement = remove(index);
+    add(element);
+    return tempElement;
   }
 }
